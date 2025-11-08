e10df255461fdc8cb811d38c917eda13ade37c13
Akbar-dev-uz <akbaryakubov12@gmail.com>
2025-11-08 08:39:29 +0500

    Base layout + static + initial templates

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'shop/index.html')
+
+
+def news_archive(request):
+    news_list = ["good"]  # News.objects.all()  # пример получения новостей
+    return render(request, 'news/archive.html', {'news_list': news_list})
